211adb3d1c8d003fe3fa5847c708c10315bc2f29
Lê Đình Dũng <ledinhdung2908@gmail.com>
2022-10-20 21:31:11 +0700

    update

---
+++ ManageStudent.h
@@ -0,0 +1,20 @@
+// Header
+
+#ifndef MANAGESTUDENT_H
+#define MANAGESTUDENT_H
+
+#include <string>
+#include <Student.h>
+using namespace std;
+
+class ManageStudent
+{
+public:
+    // Default Constructor
+    ManageStudent();
+
+    
+    ~ManageStudent();
+};
+
+#endif
